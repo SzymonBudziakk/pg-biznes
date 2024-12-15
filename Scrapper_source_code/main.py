c480ead59465a914b2cb562bcc8c3e2bca11e1a1
NoZakrze <147536257+NoZakrze@users.noreply.github.com>
2024-12-15 20:20:26 +0100

    scrapper_fix

--- Scrapper_source_code/main.py
+++ Scrapper_source_code/main.py
@@ -13,7 +13,7 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 GECKODRIVER_PATH = "C:\\Users\\nozak\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Python 3.12\\Scripts\\geckodriver.exe"
 
 firefox_options = Options()
-firefox_options.add_argument("--headless")
+#firefox_options.add_argument("--headless")
 
 def initialize_driver():
     service = Service(GECKODRIVER_PATH)
@@ -27,50 +27,57 @@ def main():
 
     try:
         Wloczykijki.scrapuj_kategorie(driver)
-        Wloczykijki.parsuj_liste_kategorii()   #lista kategorii
+        #Wloczykijki.parsuj_liste_kategorii()   #lista kategorii
         for i in range(6):
             Wloczykijki.kategorie[i].scrapuj_podkategorie(driver)
-            Wloczykijki.kategorie[i].parsuj_liste_podkategorii()    #listy podkategorii
+            #Wloczykijki.kategorie[i].parsuj_liste_podkategorii()    #listy podkategorii
 
+        '''
         #OPASKI
-        Wloczykijki.kategorie[5].podkategorie[0].scrapuj_produkty(driver)
-        Wloczykijki.kategorie[5].JSON_Podkateogria(0)
-        Wloczykijki.kategorie[5].generuj_jpg(0)
+        Wloczykijki.kategorie[5].podkategorie[0].scrapuj_produkty(driver,True)
+        Wloczykijki.kategorie[5].JSON_Podkateogria(0,True)
+        Wloczykijki.kategorie[5].generuj_jpg(0,True)
 
         #CZAPKI
-        Wloczykijki.kategorie[5].podkategorie[1].scrapuj_produkty(driver)
-        Wloczykijki.kategorie[5].JSON_Podkateogria(1)
-        Wloczykijki.kategorie[5].generuj_jpg(1)
+        Wloczykijki.kategorie[5].podkategorie[1].scrapuj_produkty(driver,True)
+        Wloczykijki.kategorie[5].JSON_Podkateogria(1,True)
+        Wloczykijki.kategorie[5].generuj_jpg(1,True)
 
+        
         #ORGANIZERY
-        Wloczykijki.kategorie[3].podkategorie[3].scrapuj_produkty(driver)
-        Wloczykijki.kategorie[3].JSON_Podkateogria(3)
-        Wloczykijki.kategorie[3].generuj_jpg(3)
+        Wloczykijki.kategorie[3].podkategorie[3].scrapuj_produkty(driver,True)
+        Wloczykijki.kategorie[3].JSON_Podkateogria(3,True)
+        Wloczykijki.kategorie[3].generuj_jpg(3,True)
 
         #TORBY
-        Wloczykijki.kategorie[3].podkategorie[4].scrapuj_produkty(driver)
-        Wloczykijki.kategorie[3].JSON_Podkateogria(4)
-        Wloczykijki.kategorie[3].generuj_jpg(4)
+        Wloczykijki.kategorie[3].podkategorie[4].scrapuj_produkty(driver,True)
+        Wloczykijki.kategorie[3].JSON_Podkateogria(4,True)
+        Wloczykijki.kategorie[3].generuj_jpg(4,True)
+        '''
 
         #GUZIKI
-        Wloczykijki.kategorie[2].podkategorie[6].scrapuj_produkty(driver)
-        Wloczykijki.kategorie[2].JSON_Podkateogria(6)
-        Wloczykijki.kategorie[2].generuj_jpg(6)
+        Wloczykijki.kategorie[2].podkategorie[6].scrapuj_produkty(driver,True)
+        Wloczykijki.kategorie[2].JSON_Podkateogria(6,True)
+        Wloczykijki.kategorie[2].generuj_jpg(6,True)
 
         #NOZYCZKI
-        Wloczykijki.kategorie[2].podkategorie[7].scrapuj_produkty(driver)
-        Wloczykijki.kategorie[2].JSON_Podkateogria(7)
-        Wloczykijki.kategorie[2].generuj_jpg(7)
+        Wloczykijki.kategorie[2].podkategorie[7].scrapuj_produkty(driver,True)
+        Wloczykijki.kategorie[2].JSON_Podkateogria(7,True)
+        Wloczykijki.kategorie[2].generuj_jpg(7,True)
 
+        '''
         #PROBKI
-        Wloczykijki.kategorie[0].podkategorie[1].scrapuj_produkty(driver)
-        Wloczykijki.kategorie[0].JSON_Podkateogria(1)
-        Wloczykijki.kategorie[0].generuj_jpg(1)
+        Wloczykijki.kategorie[0].podkategorie[1].scrapuj_produkty(driver,False)
+        Wloczykijki.kategorie[0].JSON_Podkateogria(1,False)
+        Wloczykijki.kategorie[0].generuj_jpg(1,False)
+        '''
 
         #WLOCZKI
-        Wloczykijki.kategorie[0].podkategorie[2].scrapuj_czesc_produktow(driver,65)
-        Wloczykijki.kategorie[0].JSON_Podkateogria(2)
-        Wloczykijki.kategorie[0].generuj_jpg(2)
+        Wloczykijki.kategorie[0].podkategorie[2].scrapuj_czesc_produktow(driver,65,True)
+        Wloczykijki.kategorie[0].JSON_Podkateogria(2,True)
+        Wloczykijki.kategorie[0].generuj_jpg(2,True)
+
+
 
 
     finally:
